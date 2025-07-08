712e405ccd2b4283cc2cdebd233471a6a8c3f644
Brian Augnoo <augnoobrian@gmail.com>
2025-07-08 16:19:51 +0400

    basic js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("hire me and the code god will bless you🥀");
 let targetScrollY = window.scrollY;
 let isAnimating = false;
 
-function smoothScroll() {
+const smoothScroll = () => {
   const currentY = window.scrollY;
   const delta = targetScrollY - currentY;
   window.scrollTo(0, currentY + delta * 0.1);
@@ -13,6 +13,7 @@ function smoothScroll() {
     requestAnimationFrame(smoothScroll);
   } else {
     isAnimating = false;
+    console.log("stop")
   }
 }
 
@@ -30,3 +31,39 @@ window.addEventListener('wheel', function(e) {
     requestAnimationFrame(smoothScroll);
   }
 }, { passive: false });
+
+
+let descriptionSpacing = false;
+let handmadeFeed = true;
+let carbnbFeed = false;
+
+const project = () => {
+  const description = document.querySelector(".project-description");
+  const descriptionPosition = description.getBoundingClientRect();
+  const handmadePosition = document.querySelector("#handmade").getBoundingClientRect();
+  const carbnbPosition = document.querySelector("#carbnb").getBoundingClientRect();
+  const title = description.querySelector("h3");
+  const text = description.querySelector("p");
+
+  if (descriptionPosition.top <= 100 && !descriptionSpacing ) {
+    description.classList.add("description-spacing");
+    descriptionSpacing = true;
+
+  } else if (descriptionPosition.top >= 100 && descriptionSpacing) {
+    descriptionSpacing = false;
+    description.classList.remove("description-spacing");
+  }
+
+  if (descriptionPosition.top >= carbnbPosition.top && !carbnbFeed) {
+    description.classList.add("relative-position")
+    title.textContent = "Carbnb";
+    text.textContent = "good car aPP";
+    carbnbFeed = true
+  } else if (carbnbFeed && carbnbPosition < 200) {
+    title.textContent = "Handmade"
+    text.textContent = "Best App"
+    description.classList.remove("relative-position")
+    description.classList.add("sticky-position")
+    carbnbFeed = false
+  }
+}
