49651575e5e042c0a3478cd3553bb94e81d657b9
Brian Augnoo <augnoobrian@gmail.com>
2025-07-09 16:59:59 +0400

    swicth to gasp

--- index.js
+++ index.js
@@ -1,69 +1,34 @@
-console.log("hire me and the code god will bless you🥀");
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollSmoother)
+  let smoother = ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 2,
+    smoothTouch: 0.1,
+    effects: true
+  });
 
+  const home = document.querySelector("#home");
+  const about = document.querySelector("#about");
+  const contact = document.querySelector("#contact")
 
-let targetScrollY = window.scrollY;
-let isAnimating = false;
+  home.addEventListener("click", (e) => {
+    e.preventDefault()
+    smoother.scrollTo("#intro", true, "center center");
+  })
 
-const smoothScroll = () => {
-  const currentY = window.scrollY;
-  const delta = targetScrollY - currentY;
-  window.scrollTo(0, currentY + delta * 0.1);
+  about.addEventListener("click", (e) => {
+    e.preventDefault()
+    smoother.scrollTo("#education", true, "center center")
+  })
 
-  if (Math.abs(delta) > 0.5) {
-    requestAnimationFrame(smoothScroll);
-  } else {
-    isAnimating = false;
-    console.log("stop")
-  }
-}
-
-window.addEventListener('wheel', function(e) {
-  e.preventDefault();
-  targetScrollY = Math.max(
-    0,
-    Math.min(
-      document.body.scrollHeight - window.innerHeight,
-      targetScrollY + e.deltaY
-    )
-  );
-  if (!isAnimating) {
-    isAnimating = true;
-    requestAnimationFrame(smoothScroll);
-  }
-}, { passive: false });
-
-
-let descriptionSpacing = false;
-let handmadeFeed = true;
-let carbnbFeed = false;
-
-const project = () => {
-  const description = document.querySelector(".project-description");
-  const descriptionPosition = description.getBoundingClientRect();
-  const handmadePosition = document.querySelector("#handmade").getBoundingClientRect();
-  const carbnbPosition = document.querySelector("#carbnb").getBoundingClientRect();
-  const title = description.querySelector("h3");
-  const text = description.querySelector("p");
-
-  if (descriptionPosition.top <= 100 && !descriptionSpacing ) {
-    description.classList.add("description-spacing");
-    descriptionSpacing = true;
-
-  } else if (descriptionPosition.top >= 100 && descriptionSpacing) {
-    descriptionSpacing = false;
-    description.classList.remove("description-spacing");
-  }
-
-  if (descriptionPosition.top >= carbnbPosition.top && !carbnbFeed) {
-    description.classList.add("relative-position")
-    title.textContent = "Carbnb";
-    text.textContent = "good car aPP";
-    carbnbFeed = true
-  } else if (carbnbFeed && carbnbPosition < 200) {
-    title.textContent = "Handmade"
-    text.textContent = "Best App"
-    description.classList.remove("relative-position")
-    description.classList.add("sticky-position")
-    carbnbFeed = false
-  }
-}
+  gsap.to(".show-cv", {
+    rotate: 45,
+    scrollTrigger: {
+      trigger: ".show-cv",
+      start: "top center",
+      markers: true,
+      scrub: true
+    }
+  })
+});
