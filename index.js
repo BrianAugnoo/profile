546893a5b865bf09d8ebdb1628786534bb59d7b6
Brian Augnoo <augnoobrian@gmail.com>
2025-07-11 11:18:40 +0400

    project section

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let smoother = ScrollSmoother.create({
     wrapper: "#smooth-wrapper",
     content: "#smooth-content",
-    smooth: 2,
+    smooth: 3,
     smoothTouch: 0.1,
     effects: true
   });
@@ -13,22 +13,75 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const contact = document.querySelector("#contact")
 
   home.addEventListener("click", (e) => {
-    e.preventDefault()
+    e.preventDefault();
     smoother.scrollTo("#intro", true, "center center");
   })
 
   about.addEventListener("click", (e) => {
-    e.preventDefault()
-    smoother.scrollTo("#education", true, "center center")
+    e.preventDefault();
+    smoother.scrollTo("#education", true, "center center");
   })
 
-  gsap.to(".show-cv", {
-    rotate: 45,
-    scrollTrigger: {
-      trigger: ".show-cv",
-      start: "top center",
-      markers: true,
-      scrub: true
+  gsap.fromTo(".show-cv",
+  { rotate: -5 },
+  {
+    rotate: 5,
+    duration: 0.5,
+    yoyo: true,
+    repeat: -1,
+    repeatDelay: 1,
+    ease: "power1.inOut",
+  });
+
+  gsap.from(".cards #card", {
+  scrollTrigger: {
+    trigger: ".cards",
+    toggleActions: "restart none none none",
+    start: "top 80%",
+  },
+  y: 100,
+  opacity: 0,
+  scale: 0,
+  ease: "elastic.out(0.4, 0.15)",
+  duration: 1,
+  stagger: 0.5
+  });
+
+
+
+  gsap.to(".project-description", {
+  scrollTrigger: {
+    trigger: ".project-description",
+    start: "top",
+    end: "bottom + 140vh",
+    scrub: true,
+    pin: true,
+    markers: true,
+  },
+  opacity: 1,
+  y: "20vh",
+  onUpdate: () => {
+    const handmadePosition = document.querySelector("#handmade").getBoundingClientRect();
+    const carbnbPosition = document.querySelector("#carbnb").getBoundingClientRect();
+    const description = document.querySelector(".project-description");
+    const descriptionPosition = description.getBoundingClientRect();
+
+    if (description.dataset.project === "handmade" && (carbnbPosition.top === descriptionPosition.top || carbnbPosition.top < 150)) {
+      console.log("change into carbnb");
+      description.dataset.project = "carbnb";
+      description.querySelector("h3").textContent = "-Carbnb";
+      description.classList.add("carbnb-description");
+      description.classList.remove("handmade-description");
+
+    } else if (description.dataset.project === "carbnb" && handmadePosition.bottom >= window.innerHeight / 2) {
+      console.log("change into handmade");
+      description.dataset.project = "handmade";
+      description.querySelector("h3").textContent = "-Handmade";
+      description.classList.add("handmade-description");
+      description.classList.remove("carbnb-description");
+
     }
-  })
+  }
+});
+
 });
